58ac9df416180e30875149100d3ccccfa9f2c5cc
MagomedMK <90756134+MagomedMK@users.noreply.github.com>
2022-05-18 15:47:54 +0200

    MongoDB

--- src/main/java/com/ktm/ktm_refurbished/web/views/HomeView.java
+++ src/main/java/com/ktm/ktm_refurbished/web/views/HomeView.java
@@ -2,6 +2,7 @@ package com.ktm.ktm_refurbished.web.views;
 
 import com.ktm.ktm_refurbished.security.SecurityService;
 import com.ktm.ktm_refurbished.web.layouts.StandardLayout;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import javax.annotation.security.PermitAll;
@@ -13,5 +14,13 @@ public class HomeView extends StandardLayout {
 
   public HomeView(SecurityService securityService) {
     super("Home", securityService);
+    Grid<Person> grid = new Grid<>(Person.class, false);
+    grid.addColumn(Person::getFirstName).setHeader("First name");
+    grid.addColumn(Person::getLastName).setHeader("Last name");
+    grid.addColumn(Person::getEmail).setHeader("Email");
+    grid.addColumn(Person::getProfession).setHeader("Profession");
+
+    List<Person> people = DataService.getPeople();
+    grid.setItems(people);
   }
 }
