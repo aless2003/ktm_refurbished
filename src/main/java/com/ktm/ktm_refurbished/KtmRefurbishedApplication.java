2f13f18934f6d09b3d8b2e8ae4ba352a1db68ebc
Alessandro Schwaiger <46018087+aless2003@users.noreply.github.com>
2022-05-18 08:26:31 +0200

    Initial commit

---
+++ src/main/java/com/ktm/ktm_refurbished/KtmRefurbishedApplication.java
@@ -0,0 +1,13 @@
+package com.ktm.ktm_refurbished;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtmRefurbishedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtmRefurbishedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktm/ktm_refurbished/KtmRefurbishedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktm.ktm_refurbished;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtmRefurbishedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
