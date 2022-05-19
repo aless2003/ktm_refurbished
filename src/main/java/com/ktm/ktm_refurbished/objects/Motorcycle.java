d10d980e187b45f617175c3969fb331f9922beb3
MagomedMK <90756134+MagomedMK@users.noreply.github.com>
2022-05-19 14:29:46 +0200

    MongoDB

---
+++ src/main/java/com/ktm/ktm_refurbished/objects/Motorcycle.java
@@ -0,0 +1,29 @@
+package com.ktm.ktm_refurbished.objects;
+
+
+import com.vaadin.flow.component.template.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "motorcycles")
+public class Motorcycle {
+
+  @Id
+  private String vin;
+
+  private String type;
+
+  private String name;
+
+  private Integer kilometers;
+
+  private Integer horsePower;
+
+  private Integer displacement;
+}
---
+++ src/main/java/com/ktm/ktm_refurbished/repositories/MotorcycleRepository.java
@@ -0,0 +1,10 @@
+package com.ktm.ktm_refurbished.repositories;
+
+import com.ktm.ktm_refurbished.objects.Motorcycle;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MotorcycleRepository extends MongoRepository<Motorcycle, String> {
+
+}
