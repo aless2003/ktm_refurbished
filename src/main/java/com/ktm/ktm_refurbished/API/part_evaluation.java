e579722f976ae5e6ba118ab645101854384d2cbc
Chris2300AT <82949838+Chris2300AT@users.noreply.github.com>
2022-05-19 16:55:17 +0200

    algorithmus

--- src/main/java/com/ktm/ktm_refurbished/API/part_evaluation.java
+++ src/main/java/com/ktm/ktm_refurbished/API/part_evaluation.java
@@ -2,31 +2,37 @@ package com.ktm.ktm_refurbished.API;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class part_evaluation {
 
   private String vin;
 
   private String partName;
 
-  public part_evaluation(
-      String vin, String partName)
+  private int km;
+
+  public part_evaluation(String vin, String partName, int km)
   {
     super();
-
     this.vin = vin;
-
+    this.km = km;
     this.partName = partName;
   }
 
-  public String evaluation(){
+  public float evaluation(){
     String[] factor = {"RPM", "Watertemperatur"};
-    float[] normalUserValue = {2500, 80};
+    float[] normalUserValue = {2500, 80, 70};
     String[] whatIsOptimal = {"mid", "mid"};
     float[] weighting = {50, 50};
     float[] spesificValue = {1500, 70};
+    int KMimpect = 50;
+    int KMaverage = 40000;
 
     float[] deviation = {1,1};
+    float rating = 0;
 
     for (int i = 0; i < factor.length; i++) {
       if(whatIsOptimal[i].equals("low")) {
@@ -54,10 +60,13 @@ public class part_evaluation {
           deviation[i] = 0;
         deviation[i] = 100 - deviation[i];
       }
-
+      rating = rating + ((deviation[i] * weighting[i]) / 100);
       System.out.println(deviation[i]);
     }
-    return "test";
+    rating = rating + ((KMaverage * KMimpect) / 100);
+    System.out.println("Gesamt Rating: " + rating);
+
+    return rating;
   }
 
 }
